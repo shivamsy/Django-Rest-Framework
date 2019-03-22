b5c5ef30593501f17ca0ff90311809c778e9b06d
shivamsy <shivamsingh.y16@iiits.in>
2019-03-22 11:40:42 +0530

    models created

--- bookReview/api/views.py
+++ bookReview/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+
+from . import models
 
 # Create your views here.
+def landing(request):
+	return HttpResponse('<h1>Hello</h1>')
